0765018f0565f3b0e0c05c1b1f988b34219856be
Hyewon Shin <100207457+mrRobotCorp@users.noreply.github.com>
2023-05-25 12:47:05 +0900

    sql 날짜 데이터
    sql 날짜 데이터 처리 연습 - months_between 등 mod() 활용하여 연월일 처리

--- frontWeb2/database/a12_function_date.sql
+++ frontWeb2/database/a12_function_date.sql
@@ -17,14 +17,34 @@ SELECT first_name,HIRE_DATE , SYSDATE - hire_date "근무일수",
 FROM EMPLOYEES;
 
 -- ex) 사원명과 함께 인턴기간(입사 후, 120일), 입사 8일 전, 현재일 기준으로 근무일 수 출력
-SELECT FIRST_NAME, to_char(HIRE_DATE + 120, 'YYYY/MM/DD') "인턴기간",
+SELECT FIRST_NAME, to_char(HIRE_DATE + 120, 'YYYY/MM/DD') "인턴 막날",
 		to_char(HIRE_DATE - 8, 'YYYY/MM/DD') "8일 전..", 
 		SYSDATE - HIRE_DATE "근무일 수"
 FROM EMPLOYEES;
 
+SELECT ename, hiredate,
+		MONTHS_BETWEEN(SYSDATE, hiredate) "근무 개월 수",
+		FLOOR(MONTHS_BETWEEN(SYSDATE, hiredate)) "근무 개월 수",
+		ceil(MONTHS_BETWEEN(SYSDATE, hiredate)) "근무 개월 차"
+FROM emp;
 
+-- 12로 나누면 근무 연한과 근무 연차를 계산
+-- ceil, floor 중첩적으로 활용하여 처리할 수 있음
 
+-- ex) dual을 이용하여 오늘로부터 1000일 이후 개월 수 출력
+SELECT floor(MONTHS_BETWEEN((sysdate + 1000), SYSDATE) ) "개월 수"
+FROM dual;
 
+-- ex) emp 테이블을 ename, hiredate와 위 월단위 함수를 이용해서 ##년 ##개월 근무 <- 출력
+SELECT ename, HIREDATE, floor(floor(MONTHS_BETWEEN(SYSDATE , HIREDATE)) / 12) || '년'
+		|| MOD(floor(MONTHS_BETWEEN(SYSDATE , HIREDATE)), 12) || '개월 근무' "근무일수"
+FROM emp;
+
+-- ex) 오늘로부터 근무하는 것을 기준으로 2000일 이후의 근무연한과 개월 수 출력
+-- ## 년 ##개월근무
+SELECT floor(floor(MONTHS_BETWEEN(sysdate+2000, SYSDATE)) / 12) || '년 '
+	|| MOD(floor(MONTHS_BETWEEN(SYSDATE+2000 , SYSDATE)), 12)  || '개월 근무' "근무일수"
+FROM dual;
 
 
 		

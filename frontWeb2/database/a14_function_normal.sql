4544d9633b91e60896f9817d316db85d87242c4c
Hyewon Shin <100207457+mrRobotCorp@users.noreply.github.com>
2023-06-02 16:12:21 +0900

    sql - case, sign
    sql의 조건문 case, sign 함수

--- frontWeb2/database/a14_function_normal.sql
+++ frontWeb2/database/a14_function_normal.sql
@@ -55,17 +55,63 @@ FROM emp;
 
 
 -- ex) 사원번호를 기준으로 짝수인 경우 홍팀, 홀수인 경우 청팀으로 처리.
-SELECT empno, ename, DECODE(empno, MOD(empno,2)=0, '홍팀',
-									MOD(empno,2)=1, '청팀', '') "사원번호"
+SELECT empno, ename, DECODE( MOD(empno,2), 0, '홍팀',
+									1, '청팀') "사원번호"
 FROM emp;
 
 -- ex) 사원의 직책(job)이 SALESMAN이면 보너스(comm)이 null이 아닌 경우에는
 -- 영업직, 그 외는 비영업직이라는 영업직 여부를 사원번호, 사원명, 직책, 보너스, 영업직 여부 출력
+SELECT empno, ename, job, comm,
+		DECODE(job, 'SALESMAN', DECODE(comm, NULL, '비영업직', '영업직' ), '비영업직') "result"
+FROM emp;
 
+-- ex) 부서번호에 따라서 보너스 적용
+SELECT ename, deptno, sal, 
+		CASE WHEN deptno = 10 THEN sal * 0.2
+			WHEN deptno = 20 THEN sal * 0.5
+			WHEN deptno = 30 THEN sal * 0.7
+			ELSE sal * 1.2
+			END 보너스
+FROM emp;
 
+-- ex) job에 따른 보너스 적용 CLERK 5%, PRESIDENT 10%, MANAGER 17%, ANALYST 20%
+-- 그외는 0%, 급여 합산 출력
+SELECT ename, job, sal,
+		CASE WHEN job = 'CLERK' THEN sal + (sal * 0.05)
+			WHEN job = 'PRESIDENT' THEN sal + (sal * 0.1)
+			WHEN job = 'MANAGER' THEN sal + (sal * 0.17)
+			WHEN job = 'ANALYST' THEN sal + (sal * 0.2)
+			ELSE sal
+		END 보너스포함
+FROM emp;
 
+SELECT DISTINCT job FROM emp;
 
+-- ex) 위 ex1을 switch case문 형태로 변환
+SELECT ename, job, sal,
+	CASE job WHEN 'CLERK' THEN sal + (sal * 0.05)
+			WHEN 'PRESIDENT' THEN sal + (sal * 0.1)
+			WHEN 'MANAGER' THEN sal + (sal * 0.17)
+			WHEN 'ANALYST' THEN sal + (sal * 0.2)
+	ELSE SAL 
+	END 보너스포함스위치
+FROM emp;
+	
+FROM emp;
+-- ex) sal 5000이상 A, 4000대 B, 3000대 C, 2000대 D, 2000미만 F
+-- 급여 등급 출력
 
+SELECT ename, sal,
+		CASE WHEN sal >= 5000 THEN 'A'
+				WHEN (sal >= 4000 AND sal < 5000) THEN 'B'
+				WHEN (sal >= 3000 AND sal < 4000) THEN 'C'
+				WHEN (sal >= 2000 AND sal < 3000) THEN 'D'
+				WHEN sal < 2000 THEN 'F'
+		END 등급
+FROM emp;
 
+		
+
+		
 
 		

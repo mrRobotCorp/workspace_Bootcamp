95f5c0f67ae53ac2804a3235563e8bd6e0196bc4
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-06-05 11:07:02 +0900

    .
    .

--- frontWeb2/src/main/java/project/opgg/GameHistoryMethod.java
+++ frontWeb2/src/main/java/project/opgg/GameHistoryMethod.java
@@ -8,7 +8,6 @@ import project.opgg.VO.GameHistory;
 public class GameHistoryMethod {
 	Scanner sc = new Scanner(System.in);
 	
-	// 메소드 - @전@승@패
 	public void getWinLost() {
 		ghDao gh = new ghDao();
 		String inputUser = sc.next();
@@ -32,15 +31,7 @@ public class GameHistoryMethod {
 	
 	public void getUserKA() {
 		ghDao gh = new ghDao();
-		
 		String inputUser = sc.next();
-		double kill = 0;
-		double assi = 0;
-		
-		for(int i =0; i< gh.getUserGameHistory(inputUser).size(); i++) {
-			kill =  gh.getUserGameHistory(inputUser).get(1).getKill();
-			assi =  gh.getUserGameHistory(inputUser).get(1).getAssist();
-		}
 		
 		List<GameHistory> teamAll = gh.getTeamGameHistory(gh.getUserGameHistory(inputUser).get(0).getGameCode());
 		
@@ -51,20 +42,32 @@ public class GameHistoryMethod {
 		double killAll2 = 0;
 		
 		// 각 팀 전체 킬 수 구하기
-		for(int i=0; i<5; i++) {
+		for(int i=0; i<5; i++) { // BLUE
 			killAll1 += teamAll.get(i).getKill();
 		}
-		for(int i=5; i<10; i++) {
+		for(int i=5; i<10; i++) { // RED
 			killAll2 += teamAll.get(i).getKill();
 		}
 		
-		double userKillPer = ((kill + assi) / killAll1) * 100;
+		// input user의 킬, 어시, 팀 컬러
+		double iuKill = gh.getUserGameHistory(inputUser).get(0).getKill();
+		double iuAssi = gh.getUserGameHistory(inputUser).get(0).getAssist();
+		String iuTeamColor = gh.getUserGameHistory(inputUser).get(0).getTeamColor();
+		double userKillPer = 0;
+		
+		// 입력된 유저의 팀에 따라 나눌 전체 킬 수 구하기
+		if(iuTeamColor.equals("BLUE")) {
+			userKillPer =  ((iuKill + iuAssi) / killAll1) * 100;
+		} else {
+			userKillPer =  ((iuKill + iuAssi) / killAll2) * 100;
+		}
+		
 	    int userKillPerInt = (int)(Math.round(userKillPer));
-		System.out.println("▶ 해당 유저의 킬 관여율 : " + userKillPerInt + "%, 해당 유저의 팀 : " + gh.getUserGameHistory(inputUser).get(0).getTeamColor());
+		System.out.println("▶ 해당 유저의 킬 관여율 : " + userKillPerInt + "%, 해당 유저의 팀 : " + iuTeamColor);
 		
 		System.out.println(teamColor1 + " 팀의 전체 킬 : "+ (int)killAll1);
 		
-		for(int idx=0;idx<5;idx++) { // 해당 팀원 킬 관여
+		for(int idx=0;idx<5;idx++) { // BLUE팀 킬 관여율
 		    GameHistory ugh = teamAll.get(idx);
 		    
 		    String userid = ugh.getUserId();
@@ -78,7 +81,7 @@ public class GameHistoryMethod {
 		
 		System.out.println(teamColor2 + " 팀의 전체 킬 : "+ (int)killAll2);
 		
-		for(int idx=5;idx<10;idx++) { // 상대 팀원 킬 관여
+		for(int idx=5;idx<10;idx++) { // RED팀 킬 관여율
 			GameHistory ugh = teamAll.get(idx);
 		    
 			String userid = ugh.getUserId();
@@ -97,8 +100,8 @@ public class GameHistoryMethod {
 	public static void main(String[] args) {
 		GameHistoryMethod uh = new GameHistoryMethod();
 		
-//		System.out.print("유저의 이름을 입력(킬관여율) > ");
-//		uh.getUserKA();
+		System.out.print("유저의 이름을 입력(킬관여율) > ");
+		uh.getUserKA();
 		System.out.print("유저의 이름을 입력(전승패) > ");
 		uh.getWinLost();
 		

0bcfd7d69dac969f17a804ca2beb1eccb0b2f076
Hyewon Shin <100207457+mrRobotCorp@users.noreply.github.com>
2023-06-07 17:49:34 +0900

    0607
    0607

--- frontWeb2/database/a17_join2.sql
+++ frontWeb2/database/a17_join2.sql
@@ -131,6 +131,44 @@ FROM EMP e ,
 ) me
 WHERE e.sal = me.sal;
 
+--예제0607
+--1. Group 함수 연습 예제:
+--   1.1 직업별로 평균 급여를 계산하시오.
+
+SELECT job, trunc(avg(sal),0) 평균급여 FROM emp GROUP BY job ORDER BY job;
+
+--   1.2 각 부서별로 가장 높은 급여를 받는 사람을 찾으시오.
+SELECT deptno, max(sal) FROM emp GROUP BY deptno ORDER BY deptno;
+
+SELECT deptno, max(sal) 최고급여 FROM emp GROUP BY deptno
+ORDER BY  deptno;
+
+--2. Subquery 연습 예제:
+SELECT sal 
+--   2.1 급여가 평균 급여보다 많이 받는 사원들의 이름과 급여를 찾으시오.
+SELECT e.ename, e.sal FROM emp e WHERE e.sal>(SELECT avg(sal) FROM emp);
+--   2.2 Smith의 급여보다 많은 급여를 받는 사원들의 이름과 급여를 찾으시오.
+SELECT sal FROM emp WHERE ename='SMITH';
+SELECT e.ename, e.sal FROM emp e WHERE e.sal>(SELECT sal FROM emp WHERE ename='SMITH');
+--   3.1 사원들의 이름과 부서 이름을 가져오시오. (부서7에7777 할당되지 않은 사원도 포함)
+SELECT e.ename, d.dname FROM emp e, dept d WHERE e.deptno(+)=d.deptno;
+--   3.2 각 부서별로 최대 급여를 받는 사원의 이름을 가져오시오. (부서에 할당되지 않은 사원도 포함)
+SELECT deptno, max(sal) FROM emp GROUP BY deptno;
+SELECT e.ename, e.deptno,  d.sal FROM emp e, (SELECT deptno, max(sal) sal FROM emp GROUP BY deptno) d WHERE e.DEPTNO =d.deptno
+AND e.sal = d.sal ORDER BY deptno;
+
+
+--4. Group 함수와 Subquery를 조합한 연습 예제:
+--   4.1 각 직업별로 최대 급여를 받는 사원의 이름을 가져오시오.
+SELECT job, max(sal) FROM emp GROUP BY job ORDER BY job;
+SELECT e.ename, e.job, d.sal FROM emp e, (SELECT job, max(sal) sal FROM emp GROUP BY job ORDER BY job) d
+WHERE e.sal=d.sal AND e.job=d.job;
+
+--   4.2 각 부서별로 최소 급여를 받는 사원의 이름을 가져오시오.
+SELECT deptno, min(sal) FROM emp GROUP BY deptno ORDER BY deptno;
+SELECT e.ename, e.deptno, d.sal FROM emp e, (SELECT deptno, min(sal) sal FROM emp GROUP BY deptno ORDER BY deptno) d
+WHERE e.sal=d.sal ORDER BY deptno;
+
 
 
 
